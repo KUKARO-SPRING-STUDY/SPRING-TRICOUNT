1cc45524c0d94c939896efed8a5e72081865cec4
coeat.k <coeat.k@kakaocorp.com>
2024-03-29 21:09:21 +0900

    feat: jpa 삭제

---
+++ src/main/java/groom/backend/springtricount/config/LoginCheckInterceptor.java
@@ -0,0 +1,31 @@
+package groom.backend.springtricount.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 호출 순서는 다음과 같습니다:
+ * 1. preHandle
+ * 2. 컨트롤러 메소드 실행
+ * 3. postHandle
+ * 4. 뷰 렌더링
+ * 5. afterCompletion
+ */
+public class LoginCheckInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
---
+++ src/main/java/groom/backend/springtricount/config/WebConfig.java
@@ -0,0 +1,19 @@
+package groom.backend.springtricount.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor()
+    }
+
+    @Bean
+    public LoginCheckInterceptor loginCheckInterceptor() {
+        return new LoginCheckInterceptor();
+    }
+}
